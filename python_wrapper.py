8f33104071fe9940a563b047c15fc40f50b13196
Benjamin McCreath <enbay1@gmail.com>
2018-08-08 21:35:05 -0700

    Add python wrapper implimentation example. Update resources & gitnigore

---
+++ python_wrapper.py
@@ -0,0 +1,20 @@
+import shlex
+import subprocess
+
+
+def main():
+    file_1 = "./resources/test_short.fasta"
+    file_2 = "./resources/test_short.fasta"
+    output_file_name = "output.txt"
+    matrix_dump_file_name = "dump_file.txt"
+    gap_open = 25
+    gap_extend = 1
+    diagonal = "yes"
+    command = "./build/sw_align.exe {} {} --output {} --dump {} --open {} --extend {} --diagonal {}".format(
+        file_1, file_2, output_file_name, matrix_dump_file_name, gap_open, gap_extend, diagonal)
+    print(subprocess.check_output(command).decode('utf-8'))
+    return
+
+
+if __name__ == "__main__":
+    main()
